f799439203d65311a26b4fb356b1e68b22b760cf
Rakiit <ricardoiarced@gmail.com>
2023-08-27 14:03:45 -0700

    Babel added and encapsulation completed

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 'use strict';
 
+// Mobile navigation
+function setupMobileNavigation() {
+    const btnNavEl = document.querySelector('.btn-mobile-nav');
+    const headerEl = document.querySelector('.header');
+
+    btnNavEl.addEventListener('click', function () {
+        headerEl.classList.toggle('nav-open');
+    });
+}
+
 // Send email
-const sendEmail = function () {
+function sendEmail() {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const formmessage = document.getElementById('message').value;
     const body = `
 Name: ${name} <br />
-Email: ${email}\ <br />
-Message: ${formmessage}  
+Email: ${email} <br />
+Message: ${formmessage}
     `;
+
     Email.send({
         SecureToken: '39d79899-0a51-41ef-b5d7-65ebd7f63a72',
         To: 'ricardoiarced@gmail.com',
@@ -28,31 +39,35 @@ Message: ${formmessage}
 }
 
 // Page navigation
-document.querySelector('.nav__links').addEventListener('click', function (e) {
-    e.preventDefault();
+function setupPageNavigation() {
+    document.querySelector('.nav__links').addEventListener('click', function (e) {
+        e.preventDefault();
 
-    //Matching strategy
-    if (e.target.classList.contains('nav__link')) {
-        const id = e.target.getAttribute('href');
-        document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+        if (e.target.classList.contains('nav__link')) {
+            const id = e.target.getAttribute('href');
+            document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+
+    const nav = document.querySelector('.nav');
+    const navHeight = nav.getBoundingClientRect().height;
+    const header = document.querySelector('.header');
+
+    const stickyNav = function (entries) {
+        const [entry] = entries;
+
+        if (!entry.isIntersecting) nav.classList.add('sticky');
+        else nav.classList.remove('sticky');
     }
-});
 
-const nav = document.querySelector('.nav');
-const navHeight = nav.getBoundingClientRect().height;
-const header = document.querySelector('.header');
+    const headerObserver = new IntersectionObserver(stickyNav, {
+        root: null,
+        threshold: 0,
+        rootMargin: `-${navHeight}px`,
+    });
 
-const stickyNav = function (entries) {
-    const [entry] = entries;
-
-    if (!entry.isIntersecting) nav.classList.add('sticky');
-    else nav.classList.remove('sticky');
-
+    headerObserver.observe(header);
 }
 
-const headerObserver = new IntersectionObserver(stickyNav, {
-    root: null,
-    threshold: 0,
-    rootMargin: `-${navHeight}px`,
-});
-headerObserver.observe(header);
+setupMobileNavigation();
+setupPageNavigation();
