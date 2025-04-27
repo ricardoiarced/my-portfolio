4a656740a17c17f850d2b8d2974bf06acef77c37
Ricardo Arce <ricardoiarced@gmail.com>
2025-04-27 14:53:45 -0700

    changes portfolio v1

--- script.js
+++ script.js
@@ -10,49 +10,19 @@ function setupMobileNavigation() {
     });
 }
 
-// Send email
-function sendEmail() {
-    const submit = document.getElementsByClassName('form-contact')[0];
-
-    submit.addEventListener('submit', function (e) {
-        e.preventDefault();
-
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const formmessage = document.getElementById('message').value;
-
-        const body = `
-        Name: ${name} <br />
-        Email: ${email} <br />
-        Message: ${formmessage}
-            `;
-
-        Email.send({
-            SecureToken: '39d79899-0a51-41ef-b5d7-65ebd7f63a72',
-            To: 'ricardoiarced@gmail.com',
-            From: 'ricardoiarced@gmail.com',
-            Subject: `Message from ${name} in your portfolio`,
-            Body: body
-        }).then(
-            message => {
-                if (String(message) === 'OK') {
-                    swal('Your message has been sent successfully!', 'I\'m going to send you a response back', 'success');
-                } else {
-                    swal('Something went wrong', 'Try again later', 'error');
-                }
-            }
-        );
-    })
-}
-
 // Page navigation
 function setupPageNavigation() {
+    const headerEl = document.querySelector('.header'); // Add this
     document.querySelector('.nav__links').addEventListener('click', function (e) {
         e.preventDefault();
 
         if (e.target.classList.contains('nav__link')) {
             const id = e.target.getAttribute('href');
             document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+
+            if (headerEl.classList.contains('nav-open')) {
+                headerEl.classList.remove('nav-open');
+            }
         }
     });
 
