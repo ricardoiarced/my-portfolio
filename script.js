cbd60bbcf8c6944b610ae8f79aaaff71a4fe0d62
Ricardo Arce <ricardoiarced@gmail.com>
2025-04-27 15:31:36 -0700

    portfolio changes v2

--- script.js
+++ script.js
@@ -12,14 +12,26 @@ function setupMobileNavigation() {
 
 // Page navigation
 function setupPageNavigation() {
-    const headerEl = document.querySelector('.header'); // Add this
-    document.querySelector('.nav__links').addEventListener('click', function (e) {
+    const navLinks = document.querySelector('.nav__links');
+    const headerEl = document.querySelector('.header'); // Already needed for mobile nav
+
+    navLinks.addEventListener('click', function (e) {
         e.preventDefault();
 
         if (e.target.classList.contains('nav__link')) {
             const id = e.target.getAttribute('href');
-            document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+            const element = document.querySelector(id);
 
+            const headerOffset = 90; // 9rem = 90px
+            const elementPosition = element.getBoundingClientRect().top;
+            const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+            window.scrollTo({
+              top: offsetPosition,
+              behavior: 'smooth'
+            });
+
+            // Close mobile nav menu if open
             if (headerEl.classList.contains('nav-open')) {
                 headerEl.classList.remove('nav-open');
             }
@@ -48,4 +60,3 @@ function setupPageNavigation() {
 
 setupMobileNavigation();
 setupPageNavigation();
-sendEmail();
