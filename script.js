241d03ad2cf2764a1dde2f49929b3b652b6bd77b
Ricardo Arce <ricardoiarced@gmail.com>
2025-04-27 22:21:24 -0700

    portfolio changes v3

--- script.js
+++ script.js
@@ -38,24 +38,6 @@ function setupPageNavigation() {
         }
     });
 
-    const nav = document.querySelector('.nav');
-    const navHeight = nav.getBoundingClientRect().height;
-    const header = document.querySelector('.header');
-
-    const stickyNav = function (entries) {
-        const [entry] = entries;
-
-        if (!entry.isIntersecting) nav.classList.add('sticky');
-        else nav.classList.remove('sticky');
-    }
-
-    const headerObserver = new IntersectionObserver(stickyNav, {
-        root: null,
-        threshold: 0,
-        rootMargin: `-${navHeight}px`,
-    });
-
-    headerObserver.observe(header);
 }
 
 setupMobileNavigation();
